b59233a559fd2ae8db9ab81f555b431507ae257f
nhemgomia <gomianehemiahrenel@gmail.com>
2023-08-08 18:48:31 +0800

    feat: about to skill section transition

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from "react";
 import { ScrollManager } from "./components/ScrollManager";
 import { Menu } from "./components/Menu";
 import { MotionConfig } from "framer-motion";
+import { Leva } from "leva";
+import { framerMotionConfig } from "./config";
 
 function App() {
   const [section, setSection] = useState(0);
@@ -17,19 +19,14 @@ function App() {
 
   return (
     <>
-      <MotionConfig transition={{
-        type: "spring",
-        mass: 5,
-        stiffness: 500,
-        damping: 50,
-        restDelta: 0.0001,
-      }}>
+      <MotionConfig 
+        transition={{...framerMotionConfig}}>
         <Canvas shadows camera={{ position: [0, 3, 10], fov: 42 }}>
           <color attach="background" args={["#e6e7ff"]} />
           <ScrollControls pages={4} damping={0.1}>
             <ScrollManager section={section} onSectionChange={setSection} />
             <Scroll>
-              <Experience section={section} />
+              <Experience section={section} menuOpened={menu}/>
             </Scroll>
             <Scroll html>
               <Interface />
@@ -38,6 +35,7 @@ function App() {
         </Canvas>
         <Menu onSectionChange={setSection} menuOpened={menu} setMenuOpened={setMenuOpened} />
       </MotionConfig>
+      <Leva hidden />
     </>
   );
 }
